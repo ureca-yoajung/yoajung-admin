ac0e5a50d514ded5aff72afad9df60f495ef5d40
Eddie Lee <devleehy@gmail.com>
2025-06-02 14:22:28 +0900

    init project

---
+++ src/main/java/com/ureca/yoajungadmin/YoajungAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.yoajungadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoajungAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoajungAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ureca/yoajungadmin/YoajungAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.yoajungadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoajungAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
